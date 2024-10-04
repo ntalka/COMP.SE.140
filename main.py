8e4fe07c96e28d7057f02d5219d69ea2b59b2ced
ntalka <ntalka_git@outlook.com>
2024-10-04 20:38:19 +0000

    + more graceful error handling when receiving data from service2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 
 import requests
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
 def runShell(c:str)->str:
     return os.popen(c).read()
 
-def getSystemStats():
+def getSystemStats() -> dict[str, str]:
     return  {
         "ip":  runShell("hostname -I").split(sep=" ")[0].rstrip(),
         "ps": runShell("ps -ax").split(sep='\n')[1:],
@@ -16,12 +16,15 @@ def getSystemStats():
         "uptime": runShell("uptime -p").rstrip()
 }
 
-def reqService2SystemStats():
-    r = requests.get("http://service_2_go:8200")
+def reqService2SystemStats() -> any:
+    try:
+        r = requests.get("http://service_2_go:8220")
+    except Exception as e:
+        return {"error": str(e)}
     return r.json()
 
 @app.route('/', methods=['GET'])
-def GET_SystemStats():
+def GET_SystemStats() -> Response:
     return jsonify({
         "service_1_python": getSystemStats(),
         "service_2_go": reqService2SystemStats()
